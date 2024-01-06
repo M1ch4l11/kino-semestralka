cfe91d5d6f889442403f20bd5af41e4553484f60
M1ch4l11 <lojko.michal11@gmail.com>
2024-01-06 19:19:40 +0100

    Config: Install TS for global

---
+++ src/interfaces/Film-interface.ts
@@ -0,0 +1,6 @@
+export interface Film {
+  title: String;
+  img: String;
+  description: String;
+  price: Number;
+}
---
+++ src/router/index.ts
@@ -0,0 +1,41 @@
+import { createRouter, createWebHistory } from "vue-router";
+
+const routes = [
+  {
+    path: "/",
+    name: "Home",
+    component: () => import("@/pages/MainPage.vue"),
+  },
+  {
+    path: "/film",
+    name: "Film",
+    component: () => import("@/pages/FilmPage.vue"),
+  },
+  {
+    path: "/kontakt",
+    name: "Kontakt",
+    component: () => import("@/pages/KontaktPage.vue"),
+  },
+  {
+    path: "/program",
+    name: "Program",
+    component: () => import("@/pages/ProgramPage.vue"),
+  },
+  {
+    path: "/shopping",
+    name: "Shopping",
+    component: () => import("@/pages/ShopingPage.vue"),
+  },
+  {
+    path: "/:pathMatch(.*)*",
+    name: "NotFound",
+    component: () => import("@/pages/NotFound.vue"),
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+export default router;
