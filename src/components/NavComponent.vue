5f37965abd86a92f10cb56fb29673441d342445e
M1ch4l11 <lojko.michal11@gmail.com>
2024-01-07 17:30:48 +0100

    Feat: Create NavComponent

--- src/components/NavComponent.vue
+++ src/components/NavComponent.vue
@@ -1,6 +1,8 @@
 <template>
-  <img />
   <nav>
+    <RouterLink to="/">
+      <img class="brand" src="/brand_movie-removebg-preview.png"
+    /></RouterLink>
     <RouterLink to="/">Home</RouterLink>
     <RouterLink to="/Program">Program</RouterLink>
     <RouterLink to="/Kontakt">Kontakt</RouterLink>
@@ -15,4 +17,25 @@ export default defineComponent({
 });
 </script>
 
-<style scoped></style>
+<style scoped>
+nav {
+  width: 100%;
+  height: 10vh;
+  display: flex;
+  justify-content: center;
+  background-color: var(--second-color);
+  align-items: center;
+  .brand {
+    height: 70px;
+  }
+  a {
+    text-decoration: none;
+    color: var(--text-color);
+    font-size: large;
+    margin: 5px;
+    &:hover {
+      color: var(--background-color);
+    }
+  }
+}
+</style>
