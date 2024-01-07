8770d05640ee03b846f3b1d6d362e39d724a3360
M1ch4l11 <lojko.michal11@gmail.com>
2024-01-07 17:30:08 +0100

    Feat: Create MainPage

--- src/pages/MainPage.vue
+++ src/pages/MainPage.vue
@@ -1,6 +1,30 @@
-<template>dfvfev</template>
+<template>
+  <div>
+    <template v-for="banner in BannersFields" :key="banner.id">
+      <BannerComponent
+        :description="banner.desription"
+        :price="Number(banner.price)"
+        :imagePath="banner.path"
+        :link="banner.link"
+      ></BannerComponent>
+    </template>
+  </div>
+</template>
 
-<script scoped></script>
+<script lang="ts">
+import { defineComponent } from "vue";
+import BannerComponent from "@/components/BannerComponent.vue";
+import BannersFields from "@/jsons/banners.json";
+
+export default defineComponent({
+  data() {
+    return {
+      BannersFields,
+    };
+  },
+  components: { BannerComponent },
+});
+</script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped></style>
