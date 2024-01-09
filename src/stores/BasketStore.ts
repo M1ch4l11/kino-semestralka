9adb0a6dddcc355378758bc978bdabbcdb716b6e
M1ch4l11 <lojko.michal11@gmail.com>
2024-01-10 00:53:09 +0100

    Feat: Create store

---
+++ src/stores/BasketStore.ts
@@ -0,0 +1,50 @@
+import { BasketItem, FilmItem } from "@/interfaces/Film-interface";
+import { defineStore } from "pinia";
+
+export const useBasketStore = defineStore("basket", {
+  state: (): {
+    basketItems: BasketItem[];
+    totalPrice: number;
+    filmItem: FilmItem;
+  } => ({
+    basketItems: [],
+    totalPrice: 0,
+    filmItem: {
+      id: 0,
+      title: "",
+      path: "",
+      description: "",
+      price: 0,
+      time: "",
+      link: "",
+    },
+  }),
+  actions: {
+    addItem(item: BasketItem) {
+      this.basketItems.push(item);
+    },
+    sumTutalPrice(price: number) {
+      this.totalPrice += price;
+    },
+    deleteItem(item: BasketItem) {
+      const index = this.basketItems.findIndex(
+        (basketItem) => basketItem.imagePath === item.imagePath
+      );
+      this.totalPrice = 0;
+      return index > -1 ? this.basketItems.splice(index, 1) : false;
+    },
+    $reset() {
+      this.basketItems = [];
+      this.totalPrice = 0;
+      this.filmItem = {
+        id: 0,
+        title: "",
+        path: "",
+        description: "",
+        price: 0,
+        time: "",
+        link: "",
+      };
+    },
+  },
+});
